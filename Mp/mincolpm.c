4723db20d7445c7e3c37e3ac012aae1d295cc6c5
Blade <atenlladojr@hotmail.com>
2018-03-21 16:36:36 +0100

    owo

---
+++ Mp/mincolpm.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int **reservarMemoria(int nFil, int nCol);
+void rellenaMatriz(int **matriz, int nFil, int nCol);
+void imprimeMatriz(int **matriz, int nFil, int nCol);
+int *minCol(int **matriz, int nFil, int nCol);
+void liberarMemoria(int ***matriz, int **minColArray, int nFil);
+
+int main(){
+	int **matrix, *minColArray, nFil, nCol;
+	srand(time(NULL));
+
+	printf("POR FAVOR, INTRODUZCA EL NÚMERO DE FILAS DE LA MATRIZ: ");
+	scanf("%d", &nFil);
+	printf("A CONTINUACIÓN, EL NÚMERO DE COLUMNAS DE DICHA MATRIZ: ");
+	scanf("%d", &nCol);
+
+	matrix = reservarMemoria(nFil, nCol);
+
+	rellenaMatriz(matrix, nFil, nCol);
+
+	imprimeMatriz(matrix, nFil, nCol);
+
+	minColArray = minCol(matrix, nFil, nCol);
+
+	printf("LOS VALORES MÁS PEQUEÑOS DE CADA COLUMNA SON: ");
+
+	for (int i = 0; i < nCol; i++){
+		printf("%d", *(minColArray + i));
+		if(i < nCol - 1) printf(", ");
+	}
+	printf(".\n");
+
+	liberarMemoria(&matrix, &minColArray, nFil);
+
+	return 0;
+}
+
+int **reservarMemoria(int nFil, int nCol){
+	int **auxMatrix = (int**) malloc(nFil * sizeof(int*));
+
+	for (int i = 0; i < nFil; i++)
+		*(auxMatrix + i) = (int*) malloc(nCol * sizeof(int));
+
+	return auxMatrix;
+}
+
+void rellenaMatriz(int **matriz, int nFil, int nCol){
+	printf("ASIGNAMOS UN VALOR ENTRE 1 Y 20 A CADA POSICION DE LA MATRIZ:\n");
+	for(int i = 0; i < nFil; i++)
+		for (int j = 0; j < nCol; j++)
+			*(*(matriz+i)+j) = rand()%20 + 1;
+}
+
+void imprimeMatriz(int **matriz, int nFil, int nCol){
+	printf("LA MATRIZ (%dx%d) HA QUEDADO:\n", nFil, nCol);
+	for(int i = 0; i < nFil; i++){
+		for (int j = 0; j < nCol; j++)
+			printf("M[%d][%d]: %d 	", i, j, *(*(matriz+i)+j));
+		printf("\n");
+	}
+}
+
+int *minCol(int **matriz, int nFil, int nCol){
+	int *auxMinCol = (int*) malloc(nCol * sizeof(int));
+
+	for(int i = 0; i < nCol; i++){
+		*(auxMinCol + i) = *(*(matriz)+i);
+		for (int j = 0; j < nFil; j++)
+			if(*(*(matriz+j)+i) < *(auxMinCol + i)) *(auxMinCol + i) = *(*(matriz+j)+i);
+	}
+	return auxMinCol;
+}
+
+void liberarMemoria(int ***matriz, int **minColArray, int nFil){
+	for(int i = 0; i < nFil; i++)
+		free((*matriz)[i]);
+	free(*matriz);
+	(*matriz) = NULL;
+
+	free(*minColArray);
+	(*minColArray) = NULL;
+}
